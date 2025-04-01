230a168392f13a0548e38ddf835e9dd5d02eafc3
Surendar <surendarh3@gmail.com>
2025-04-01 07:16:04 +0530

    update

--- backend/app.ts
+++ backend/app.ts
@@ -1,33 +1,37 @@
-import path from 'path'
-import express from 'express'
-import {Request, Response,NextFunction} from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import Post from './models/post.js'
-import postsRoutes from './routes/posts.js' 
-import userRoutes from './routes/user.js'
+import path from 'path';
+import express from 'express';
+import { Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import Post from './models/post.js';
+import postsRoutes from './routes/posts.js';
+import userRoutes from './routes/user.js';
 const app = express();
-
 
 const secretKey = process.env['MONGO_ATLAS_PW'];
 
-mongoose.connect("mongodb+srv://admin:"+secretKey+"@cluster0.wdvb3.mongodb.net/node-angular?retryWrites=true&w=majority&appName=Cluster0")
-    .then(()=> {
-        console.log("Connected to database!");
-    })
-    .catch(()=>{
-        console.log('Connection failed');
-    })
+mongoose
+  .connect(
+    'mongodb+srv://admin:' +
+      secretKey +
+      '@cluster0.wdvb3.mongodb.net/node-angular?retryWrites=true&w=majority&appName=Cluster0'
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed');
+  });
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended : false}));
-app.use("/images", express.static(path.join("backend/images")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('backend/images')));
 
-app.use(cors())
+app.use(cors());
 
-app.use((req : Request , res : Response , next : NextFunction) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
@@ -40,7 +44,7 @@ app.use((req : Request , res : Response , next : NextFunction) => {
   next();
 });
 
-app.use("/api/posts",postsRoutes)
-app.use("/api/user",userRoutes)
+app.use('/api/posts', postsRoutes);
+app.use('/api/user', userRoutes);
 
 export default app;
