dff5bee4ade463f448bf55ca8f9f0330b1ce8bfc
Surendar <surendarh3@gmail.com>
2025-04-01 07:23:54 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { PostListComponent } from './posts/post-list/post-list.component.js';
 import { PostCreateComponent } from './posts/post-create/post-create.component.js';
 
 import { AuthGuard } from './auth/auth.guard.js';
+import { RenderMode } from '@angular/ssr';
 
 const routes: Routes = [
   {path : '' , component : PostListComponent},
   {path: 'create', component : PostCreateComponent , canActivate: [AuthGuard]},
-  {path: 'edit/:postId', component : PostCreateComponent , canActivate: [AuthGuard]},
+  {path: 'edit/:postId', component : PostCreateComponent , canActivate: [AuthGuard], data : {RenderMode : 'dynamic'}},
   {path : "auth",loadChildren : () => import('./auth/auth.module').then(m => m.AuthModule)}
   
 ];
